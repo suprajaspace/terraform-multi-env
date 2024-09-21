4dcdb02f8b3a608ec303725e3ba7ebc0309cfde7
suprajaspace <supra556699@gmail.com>
2024-09-21 23:33:22 +0800

    Update ec2.tf

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "expense" {
  
   ami = "ami-09c813fb71547fc4f"
   instance_type = lookup(var.instance_type, terraform.workspace)
-  vpc_security_group_ids = ["sg-0c113dab3d5a1ebe1"]
+  vpc_security_group_ids = ["sg-xxxxxxxxxxxxxx"]
 
     tags = {
             Name = "terraform-${terraform.workspace}"
